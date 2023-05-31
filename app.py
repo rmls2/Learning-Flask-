f3730df16af03cdd1774f4c86db999fb8f8f7fac
Robert Miller <robert.miller@ufpe.br>
2023-05-31 18:01:50 -0300

    .

--- app.py
+++ app.py
@@ -5,3 +5,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+def teste_1():
+    return "<h2> teste_1</h2>"
+
+def teste_2():
+    return "<h2> teste_2</h2>"
+
+app.add_url_rule('/teste', 'teste_1', teste_1)
+
+# Criando url dinâmica 
+
+#essa rota mapeia a func_dinamica() para o valor default
+@app.route('/hello')
+@app.route('/hello/<nome>/<int:postID>')
+@app.route('/hello/<nome>/')
+def func_dinamica(nome='', postID= -1):
+
+    if postID > 0:
+        return f'esse é o meu numero da sorte: {postID}'
+    else:
+        return f'Olá, {nome}'
