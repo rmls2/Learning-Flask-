2ad3a6f665572888e889a1272b0177f5ca75df81
rmls2 <millerrobert182@gmail.com>
2023-06-01 09:38:59 -0300

    alteração gitignore, app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -12,12 +12,13 @@ def teste_1():
 def teste_2():
     return "<h2> teste_2</h2>"
 
-app.add_url_rule('/teste', 'teste_1', teste_1)
+# essa função mapeia a url /teste para o retorno da função teste_1 
+app.add_url_rule('/teste', 'teste_2', teste_1)
 
-# Criando url dinâmica 
+# Criando url dinâmica - Aula 3 
 
 #essa rota mapeia a func_dinamica() para o valor default
-@app.route('/hello')
+@app.route('/hello/')
 @app.route('/hello/<nome>/<int:postID>')
 @app.route('/hello/<nome>/')
 def func_dinamica(nome='', postID= -1):
@@ -26,3 +27,26 @@ def func_dinamica(nome='', postID= -1):
         return f'esse é o meu numero da sorte: {postID}'
     else:
         return f'Olá, {nome}'
+    
+# construção de url - Aula 4 
+
+@app.route('/admin/<adm_name>')
+def admin(adm_name):
+    return "oi, administrador %s."%adm_name 
+
+@app.route('/guest/<guest_name>')
+def guest(guest_name):
+    return f"oi, usuário %s."%guest_name
+
+@app.route ('/user/<name>/<adm_name>')
+@app.route ('/user/<name>/')
+def user (name, adm_name=''):
+    if name == 'admin':
+        return redirect(url_for('admin', adm_name = adm_name))
+    else:
+        return redirect(url_for('guest', guest_name = name))
+    
+# a função redirect() redireciona a url mapeada para uma url específica, nesse caso, paara o google.com
+@app.route('/google')
+def redirec_google():
+    return redirect('https://www.google.com/') 
